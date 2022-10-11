51cd24b9017db8144d523addb8874801c2cb58ca
Gian Daño <aislegian@gmail.com>
2022-10-11 15:25:50 +0800

    All-in-One CT

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Contact Tracing</title>
+    <meta name="viewport" content="width=device-width, initial-scale-1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Gemunu+Libre:wght@500&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <nav>
+      {{-- <label>All-in-One Contact Tracing System</label> --}}
+      <img src="{{asset('images/logo.png')}}">
+      <p>All-in-One Contact Tracing</p>
+      <ul>
+        <li><a class="active" href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/account">Account</a></li>
+        <li><a href="/feedback">Feedback</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  </body>
+</html>
